393fe2360ce1121a1cdc8d5d4d0aac67ecb0248b
zzetao <932629703@qq.com>
2018-03-03 09:07:22 +0800

    [Flow] declare module -> history

--- flow-typed/module_vx.x.x.js
+++ flow-typed/module_vx.x.x.js
@@ -1,3 +1,10 @@
 declare module 'module' {
   declare module.exports: any;
 }
+
+// history module
+declare module 'history' {
+  declare export function createBrowserHistory(props?: { }): any;
+  declare export function createHashHistory(props?: { }): any;
+  declare export function createMemoryHistory(props?: { }): any;
+}
